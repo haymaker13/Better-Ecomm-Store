149c55c8080024e443320631f100e36623a5f8cf
Aaron Hay <haymaker13@hotmail.com>
2013-11-20 14:08:00 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommStoreV2::Application.config.secret_token = '48ba3b59e6259bb3ff74a6b6ac3f1ddc480a07366faa247d66a966693bc2b3ce0bba48a6786c367fcca558bb5ce9cc097cf63e5d1f8f0e38180194ed360ac69e'
